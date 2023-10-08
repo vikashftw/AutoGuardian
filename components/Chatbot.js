2f57a6ebceda4551d7c3145ba8934f56583844c1
MohamedHAbdelnaby <mohamedhanykamal214@gmail.com>
2023-10-07 19:55:45 -0500

    Added UI for the chatbot and the chatbot functions

---
+++ components/Chatbot.js
@@ -0,0 +1,96 @@
+import React, { useState, useEffect } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  FlatList,
+  KeyboardAvoidingView,
+  Platform
+} from 'react-native';
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import styles from './styles';
+
+const Chatbot = () => {
+  const [message, setMessage] = useState('');
+  const [chatHistory, setChatHistory] = useState([]);
+
+  useEffect(() => {
+    // Fetch chat history from storage when the component mounts
+    const fetchChatHistory = async () => {
+      try {
+        const storedHistory = await AsyncStorage.getItem('chatHistory');
+        if (storedHistory) setChatHistory(JSON.parse(storedHistory));
+      } catch (error) {
+        console.error("Error fetching chat history from storage:", error);
+      }
+    };
+
+    fetchChatHistory();
+  }, []);
+
+  const handleSend = async () => {
+    if (message.trim() === '') return;
+
+    const userMessage = { type: 'user', text: message.trim() };
+    setChatHistory(currentHistory => [...currentHistory, userMessage]);
+
+    try {
+      const response = await axios.post('https://api.openai.com/v1/engines/davinci/completions', {
+        prompt: message,
+        max_tokens: 150,
+      }, {
+        headers: {
+          'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+          'Content-Type': 'application/json',
+        },
+      });
+
+      const botResponseText = response.data.choices[0].text.trim();
+      const botMessage = { type: 'bot', text: botResponseText };
+      setChatHistory(currentHistory => [...currentHistory, userMessage, botMessage]);
+
+      // Store the updated chat history
+      await AsyncStorage.setItem('chatHistory', JSON.stringify([...chatHistory, userMessage, botMessage]));
+
+    } catch (error) {
+      console.error("Error fetching response:", error);
+    }
+
+    setMessage('');
+  };
+
+  const renderItem = ({ item }) => (
+    <View style={item.type === 'user' ? styles.userMessage : styles.botMessage}>
+      <Text>{item.text}</Text>
+    </View>
+  );
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={chatHistory}
+        renderItem={renderItem}
+        keyExtractor={(item, index) => index.toString()}
+        style={styles.messageContainer}
+      />
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={styles.inputContainer}
+      >
+        <TextInput
+          style={styles.input}
+          value={message}
+          onChangeText={setMessage}
+          placeholder="Type your message..."
+        />
+        <TouchableOpacity style={styles.sendButton} onPress={handleSend}>
+          <Text style={styles.sendButtonText}>Send</Text>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+    </View>
+  );
+}
+
+export default Chatbot;
---
+++ styles/styles.js
@@ -0,0 +1,50 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  messageContainer: {
+    padding: 10,
+  },
+  userMessage: {
+    alignSelf: 'flex-end',
+    backgroundColor: 'red',
+    padding: 10,
+    borderRadius: 10,
+    marginBottom: 10,
+    maxWidth: '80%',
+  },
+  botMessage: {
+    alignSelf: 'flex-start',
+    backgroundColor: '#e6e6e6',
+    padding: 10,
+    borderRadius: 10,
+    marginBottom: 10,
+    maxWidth: '80%',
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    padding: 10,
+    borderTopWidth: 1,
+    borderTopColor: '#ddd',
+    alignItems: 'center',
+  },
+  input: {
+    flex: 1,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'red',
+    borderRadius: 10,
+  },
+  sendButton: {
+    marginLeft: 10,
+    backgroundColor: 'red',
+    padding: 10,
+    borderRadius: 20,
+  },
+  sendButtonText: {
+    color: 'white',
+  },
+});
